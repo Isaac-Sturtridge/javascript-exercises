cdb157195af09738aa4072c7305ee7636148baf6
izzy <eyes330@yahoo.co.uk>
2022-01-19 14:28:37 +0800

    completed sum exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    if(first < 0 || second < 0 || typeof first != "number" || typeof second != "number") return "ERROR"
+    let min = Math.min(first, second);
+    let max = Math.max(first, second);
+    let sum = 0, i = min;
+    while (i <=max) {
+        sum += i;
+        i += 1;
+    }
+    return sum;
 };
 
 // Do not edit below this line
