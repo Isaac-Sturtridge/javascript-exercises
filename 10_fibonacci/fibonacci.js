33950c22c54445d208af5b5ae5f8b9c01ecce752
izzy <eyes330@yahoo.co.uk>
2022-03-05 13:40:21 +0800

    built fibonacci calc

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibIndex) {
+    if(fibIndex < 0) return "OOPS"
+    let fibonacci = [0, 1, 1, 2, 3]
+    if(fibonacci[fibIndex]) {
+        return fibonacci[fibIndex]
+    } else {
+        while(fibonacci.length <= fibIndex) {
+            let first = fibonacci[fibonacci.length - 2]
+            let second = fibonacci[fibonacci.length - 1]
+            fibonacci.push(first + second)
+            console.log(fibonacci)
+        }
+        return fibonacci[fibIndex]
+    }
 };
 
 // Do not edit below this line
