043d7adb13dfa90fc9296a5ca5daa3f05e391d9b
izzy <eyes330@yahoo.co.uk>
2022-03-05 10:32:41 +0800

    calculator completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,25 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce(((currentTotal, prevTotal) => currentTotal + prevTotal), 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce(((currentTotal, prevTotal) => currentTotal * prevTotal), 1)
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+  return Math.pow(num, pow)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	return num < 0 ? -1 : num == 0 ? 1 : num * factorial(num - 1)
 };
 
 // Do not edit below this line
