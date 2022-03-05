a5cff2657a8bc46fa9460e6e2b6c4dfe7d226ae2
izzy <eyes330@yahoo.co.uk>
2022-03-05 13:22:46 +0800

    completed palindrom

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let preppedStr = str.toLowerCase().replace(/\W{1,}/g, "");
+    console.log(preppedStr)
+    for(let i = 0; i < preppedStr.length; i++) {
+        if(preppedStr[i] != preppedStr[preppedStr.length - i - 1]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
