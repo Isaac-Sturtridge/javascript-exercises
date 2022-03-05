105e4e56ca6dbdf180c90a16669b911129a5e4d5
izzy <eyes330@yahoo.co.uk>
2022-03-05 14:25:54 +0800

    killed all living people

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let ages = [];
+    let currentYear = (new Date()).getFullYear()
+    people.forEach((person) => {
+        if(!person.yearOfDeath) {
+            person.yearOfDeath = currentYear
+        }
+        let lifespan = person.yearOfDeath - person.yearOfBirth;
+        ages.push(lifespan)
+    })
+    return people[ages.indexOf(Math.max(...ages))]
 };
 
 // Do not edit below this line
